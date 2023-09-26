d5422d310a3ca2f5d85926a0365d0bcd0e934d1e
Lucas Masiero <lucas.masi.ero@hotmail.com>
2023-09-26 14:59:53 -0300

    feat: new code

--- example-1/main.go
+++ example-1/main.go
@@ -2,77 +2,52 @@ package main
 
 import "fmt"
 
-// ______________________________________________________________
 // User is a representation of a user
 type User struct {
-	Name  string
+	FirstName  string
+	LastName   string
 	Email string
 }
-func (u User) UserInfo() string {
-	return u.Name + " " + u.Email
+func (u User) FullName() string {
+	return u.FirstName + " " + u.LastName
 }
 
-// ______________________________________________________________
 // Address is a representation of a Address
 type Address struct {
 	City    string
 	State   string
 	Country string
 }
-func (a Address) AddressInfo() string {
+func (a Address) Info() string {
 	return fmt.Sprintf("%s, %s, %s", a.City, a.State, a.Country)
 }
 
-// ______________________________________________________________
-// Customer is a representation of a Customer
+// Customer is an struct representing a customer
 type Customer struct {
-	User			// Embedding
-	Address Address // Embedding
-	CUI     string
+	// User represents a user
+	User
+	// Addr represents a location of a customer
+	Addr Address
 }
-
-func (c Customer) UserInfo() string {
-	msg := "Customer: " + c.User.UserInfo()
-	return msg
-}
-
-func (c Customer) Info() string {
-	return c.User.UserInfo() + " " + c.Address.AddressInfo()
-}
-
-// ______________________________________________________________
-type Order struct {
-	Customer
-	OrderID int
-	Address
-}
-
-type PersonAttributes struct {
-	Name string
-	Age int
-	Address Address
-}
-type Person struct {
-	// Id int
-	Id int
-	// Attributes PersonAttributes
-	PersonAttributes
-}
-
 
 func main() {
-	ct := Customer{
-		User: User{
-			Name:  "John Doe",
-			Email: "johndoe@hotmail.com",
-		},
-		Address: Address{
-			City:    "New York",
-			State:   "New York",
-			Country: "USA",
-		},
+	user := User{
+		FirstName: "John",
+		LastName:  "Doe",
+		Email: "johndoe@gmail.com",
 	}
 
-	// fmt.Println(ct.Info())
-	fmt.Println(ct.UserInfo())
+	addr := Address{
+		City:    "Capital Federal",
+		State:   "Buenos Aires",
+		Country: "Argentina",
+	}
+
+	customer := Customer{
+		User: user,
+		Addr: addr,
+	}
+
+	fmt.Println("Fullname:", customer.FullName())
+	fmt.Println("Location:", customer.Addr.Info())
 }
